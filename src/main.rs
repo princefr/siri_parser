7e4f82fa42accfd7ea24282daf4924e6a8453f18
Pondonda <pondonda@gmail.com>
2024-10-14 14:22:03 +0200

    add quick

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use parser::models::envelope::Envelope;
 
 pub fn main(){
-    let envelope_resut = Envelope::from_file("src/fixtures/siri_et_xml_tn/trip_add.xml");
+    let envelope_resut = Envelope::from_file("src/fixtures/siri_et_xml_tn/siri-destineo-et-sbx.xml");
     assert!(envelope_resut.is_ok());
     println!("Trip Add XMLEnvelope: {:?}", envelope_resut.unwrap());
 }
